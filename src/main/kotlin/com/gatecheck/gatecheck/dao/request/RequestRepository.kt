ac1e13b20f4432168e1feff4bc833d8f1a723626
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-10-27 18:50:35 +0200

    for merge

--- src/main/kotlin/com/gatecheck/gatecheck/dao/request/RequestRepository.kt
+++ src/main/kotlin/com/gatecheck/gatecheck/dao/request/RequestRepository.kt
@@ -2,4 +2,6 @@ import com.gatecheck.gatecheck.model.entity.Request
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.*
 
-interface RequestRepository: MongoRepository<Request,UUID>{}
+interface RequestRepository: MongoRepository<Request,UUID>{
+    fun findById(id:UUID):Request?
+}
