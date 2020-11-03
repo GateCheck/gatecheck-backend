5c633761e8a490df8a37d4b1a65369896d6bc0e2
Heknon <development@heknon.com>
2020-11-03 18:26:20 +0200

    implemented auth controller

--- src/main/kotlin/com/gatecheck/gatecheck/api/AuthController.kt
+++ src/main/kotlin/com/gatecheck/gatecheck/api/AuthController.kt
@@ -1,22 +1,28 @@
 package com.gatecheck.gatecheck.api
 
+import com.gatecheck.gatecheck.api.template.AuthRequest
+import com.gatecheck.gatecheck.api.template.AuthResponse
+import com.gatecheck.gatecheck.model.entity.User
 import com.gatecheck.gatecheck.service.AuthService
 import com.gatecheck.gatecheck.utils.Routes
+import org.jetbrains.annotations.NotNull
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(Routes.Auth.BASE)
+@RequestMapping(Routes.BASE + Routes.Auth.BASE)
 class AuthController @Autowired constructor(private val authService: AuthService) {
-    @PostMapping
-    fun login() {
-
+    @PostMapping(Routes.Auth.LOGIN)
+    fun login(@RequestBody @Validated @NotNull authRequest: AuthRequest): AuthResponse {
+        return AuthResponse(authService.login(authRequest), true)
     }
 
     @PostMapping(Routes.Auth.REGISTER)
-    fun register() {
-
+    fun register(@RequestBody @Validated @NotNull user: User): AuthResponse {
+        return AuthResponse(authService.register(user), true)
     }
 }
---
+++ src/main/kotlin/com/gatecheck/gatecheck/api/template/AuthResponse.kt
@@ -0,0 +1,9 @@
+package com.gatecheck.gatecheck.api.template
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class AuthResponse @JsonCreator constructor(
+        @JsonProperty("token") val token: String,
+        @JsonProperty("success") val success: Boolean
+)
