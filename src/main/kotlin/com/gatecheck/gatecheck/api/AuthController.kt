2c89e00797733f93432d7b88c6073351f2b887f2
Heknon <development@heknon.com>
2020-11-10 19:04:23 +0200

    error messages fix

--- src/main/kotlin/com/gatecheck/gatecheck/api/AuthController.kt
+++ src/main/kotlin/com/gatecheck/gatecheck/api/AuthController.kt
@@ -9,6 +9,8 @@ import com.gatecheck.gatecheck.service.AuthService
 import com.gatecheck.gatecheck.utils.Routes
 import org.jetbrains.annotations.NotNull
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -16,7 +18,6 @@ import org.springframework.web.server.ResponseStatusException
 @RestController
 @RequestMapping(Routes.BASE + Routes.Auth.BASE)
 class AuthController @Autowired constructor(private val authService: AuthService) {
-    @ExceptionHandler
     @PostMapping(Routes.Auth.LOGIN)
     fun login(@RequestBody @Validated @NotNull authRequest: AuthRequest): Response {
         return try {
@@ -26,15 +27,23 @@ class AuthController @Autowired constructor(private val authService: AuthService
         }
     }
 
-    @ExceptionHandler
     @PostMapping(Routes.Auth.REGISTER)
     fun register(@RequestBody @Validated @NotNull user: Student): Response {
         return try {
             AuthResponse(authService.register(user), true)
         } catch (e: ResponseStatusException) {
-            SuccessResponse(false, e.status.value(), "REQUIRED: username, password, name, email, school. Password must be between 8 and 32 characters. Email must be valid. Username must be between 3 and 16 characters.")
+            SuccessResponse(false, e.status.value(), e.reason)
         } catch (e: Exception) {
             SuccessResponse(false, 401, if (e.message?.contains("username") == true) "Username already exists!" else "Email already exists!")
         }
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    fun missingFieldsException(ex: HttpMessageNotReadableException): SuccessResponse {
+        return SuccessResponse(false, 400, "Missing field '" + ex.message!!.substring(
+                ex.message!!.indexOf("value failed for JSON property ") + "value failed for JSON property ".length,
+                ex.message!!.indexOf("due to missing (therefore NULL) value") - 1
+        ) + "' - This field is required.")
     }
 }
