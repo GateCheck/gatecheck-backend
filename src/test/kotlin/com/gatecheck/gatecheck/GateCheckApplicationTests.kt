8e00925e2619837c26f6e92f048f42dab1d38ada
GideonMax <45212668+GideonMax@users.noreply.github.com>
2021-03-15 17:09:46 +0200

    started testing

--- src/test/kotlin/com/gatecheck/gatecheck/GateCheckApplicationTests.kt
+++ src/test/kotlin/com/gatecheck/gatecheck/GateCheckApplicationTests.kt
@@ -1,13 +1,30 @@
 package com.gatecheck.gatecheck
 
+import com.gatecheck.gatecheck.utils.Routes
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import com.gatecheck.gatecheck.model.entity.*
+import org.springframework.boot.test.web.client.postForObject
+import java.util.*
 
 @SpringBootTest
 class GateCheckApplicationTests {
-
+	val testRestTemplate: TestRestTemplate= TestRestTemplate(TestRestTemplate.HttpClientOption.ENABLE_COOKIES);
 	@Test
-	fun contextLoads() {
+	fun registerUser() {
+		val user=Student(UUID.randomUUID(),
+				"name",
+				"username",
+				"Example@gmail.com",
+				"P4ssword",
+				"/profile/username",
+				"he",
+				null,
+				null,
+				"school");
+		val response=testRestTemplate.postForObject(Routes.BASE + Routes.Auth.BASE+Routes.Auth.REGISTER,user, Any::class.java);
 	}
 
 }
